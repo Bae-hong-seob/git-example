88f001561e86b7711f47e255c9e79ad18839b5cf
sobeee <bhs8931@gmail.com>
2019-05-07 16:54:48 +0900

    수정본

--- gitexample.java
+++ gitexample.java
@@ -6,6 +6,8 @@ public class gitexample {
 		System.out.println("오픈소스 소프트웨어 2차 보고서");
 		System.out.println("깃허브 사용하기");
 		System.out.println("서두옥 교수님 화이팅 ^^*");
+		
+		System.out.println("수정본");
 	}
 
 }
